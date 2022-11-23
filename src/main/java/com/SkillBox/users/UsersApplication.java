1034ec0bed3aa515b3cdd742d79fdcf0b2e932fb
pashkov.vv <VicPas2020@yandex.ru>
2022-11-23 13:12:02 +0300

    hide unnecessary code for test compatibility (can be deleted)

--- src/main/java/com/SkillBox/users/UsersApplication.java
+++ src/main/java/com/SkillBox/users/UsersApplication.java
@@ -6,6 +6,7 @@ import com.SkillBox.users.repository.UserPageRepository;
 import com.SkillBox.users.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,61 +28,59 @@ public class UsersApplication {
 		SpringApplication.run(UsersApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner runner(UserRepository repo) {
-		return (args) -> {
-
-			//repo.deleteById(UUID.fromString("039fb3b6-6693-11ed-9022-0242ac120002"));
-
-
-			logger.info("________________All users___________________________");
-			List<User> all = repo.findAll();
-
-
-			for(User user: all){
-				logger.info(user.toString());
-			}
-
-
-			logger.info("___________________Skills________________________");
-
-			List<User> moscow = repo.findUserByCurrentLocation("Moscow");
-
-
-			for(User user: moscow){
-				user.getSkills().stream().map(HardSkills::getHardSkill).forEach(logger::info);
-			}
-
-
-			logger.info("________________Subscribers___________________________");
-
-			Optional<User> byId = repo.findById(UUID.fromString("039fb3b6-6693-11ed-9022-0242ac120002"));
-
-
-			if(byId.isPresent()) {
-				Set<User> subscribers = byId.get().getSubscribers();
-
-				subscribers.stream().map(User::getLastName).forEach(logger::info);
-			}
-		};
-	}
+//	@Bean
+//	public CommandLineRunner runner(UserRepository repo) {
+//		return (args) -> {
+//
+//
+//			logger.info("________________All users___________________________");
+//			List<User> all = repo.findAll();
+//
+//
+//			for(User user: all){
+//				logger.info(user.toString());
+//			}
+//
+//
+//			logger.info("___________________Skills________________________");
+//
+//			List<User> moscow = repo.findUserByCurrentLocation("Moscow");
+//
+//
+//			for(User user: moscow){
+//				user.getSkills().stream().map(HardSkills::getHardSkill).forEach(logger::info);
+//			}
+//
+//
+//			logger.info("________________Subscribers___________________________");
+//
+//			Optional<User> byId = repo.findById(UUID.fromString("039fb3b6-6693-11ed-9022-0242ac120002"));
+//
+//
+//			if(byId.isPresent()) {
+//				Set<User> subscribers = byId.get().getSubscribers();
+//
+//				subscribers.stream().map(User::getLastName).forEach(logger::info);
+//			}
+//		};
+//	}
 
 
-	@Bean
-	public CommandLineRunner runner2(UserPageRepository repo) {
-		return (args) -> {
-
-			logger.info("======================= PAGE 1 =================================");
-			Pageable first = PageRequest.of(0, 1);
-			List<User> one = repo.findUserByCurrentLocation("Moscow", first);
-
-			logger.info(one.toString());
-			logger.info("======================= PAGE 2 =================================");
-
-			Pageable second = PageRequest.of(1, 1);
-			List<User> two = repo.findUserByCurrentLocation("Moscow", second);
-			logger.info(two.toString());
-
-		};
-	}
+//	@Bean
+//	public CommandLineRunner runner2(UserPageRepository repo) {
+//		return (args) -> {
+//
+//			logger.info("======================= PAGE 1 =================================");
+//			Pageable first = PageRequest.of(0, 1);
+//			List<User> one = repo.findUserByCurrentLocation("Moscow", first);
+//
+//			logger.info(one.toString());
+//			logger.info("======================= PAGE 2 =================================");
+//
+//			Pageable second = PageRequest.of(1, 1);
+//			List<User> two = repo.findUserByCurrentLocation("Moscow", second);
+//			logger.info(two.toString());
+//
+//		};
+//	}
 }
